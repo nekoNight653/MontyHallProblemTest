5f852f5581b806a5d209500c1112ac89b764df97
Mabon Josephsen <catwings76@gmail.com>
2024-05-02 20:46:07 -0700

    Statistics added

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,21 @@ public class Main {
     private static final String prize = "Good investment";
     private static final String loss = "Goat";
 
-    //I made this entire thing because there was some weird thing about picking from three doors then one of them gets revealed,
-    // and then you get to pick again. Now supposedly if you switch you have a 33% chance to be right or something,
-    // but that sounds like carrying too much old data to me. SO with this I'll be able to test it
+    //I made this test of the monty hall problem
 
     //I link this video
     //https://www.youtube.com/watch?v=4Lb-6rxZxx0
-    //It's the Monty Hall Problem
+    //It's the breakdown of Monty Hall Problem
 
-    //It all just seems like applying new data wrong to me.
-    //I feel like the instant the new information of the door revealed comes in it nullifies the old information
-
-    ///Note the number of slashes in comments up here is intentional the more slashes the later into development the comment was added
-    ///Two slashes for before I tested it
-    ///Three for after I tested it, and I'm sure if I update the randomness I will test again,
-    /// and add a fourth slash for any comments after that
-
-    ///Um, update, I actually think it somehow works.. huh, that's quite magical well I'm glad to have learned!
-    ///I mean I guess that means I won since I got new information
-    ///I tested this program with a hundred doors and I seemed to win most fo the time if I switched,
-    /// and loose most of the time I didn't switch.
-
-    ///Even when I tried to skew the results by knowing my program produces consistently over hundreds of thousands tests
-    /// an average for the prize location to be behind door 66 it still seemed to work as it would suggest
-    /// (Well not quite as much but I Was Intentionally Skewing The Results)
-
-    ///Though I am curious what the mode of my prize location is over hundreds of thousands of random prize locations
-    ///Probably 100, but you never know
-
-    ///Though I think this program tends to produce the prize at higher numbers more often,
-    /// it still seems to be under the door I don't choose even when that door is lower
-
-    ///Makes me wonder though if you could take a 50 50 get a friend to put extra doors without you knowing which is which,
-    /// and then have you choose and then the friend reveals the doors that she put,
-    /// and then if you didn't choose one of their doors you have a higher probability of getting the prize?
-
-    ///Like surely there's some way to manipulate this?
+    private static final String switched = "did switch";
+    private static final String didntSwitch = "didn't switch";
+    private static final HashMap<String, Integer> timesSwitched = new HashMap<>();
+    private static final HashMap<String, Integer> timesWon = new HashMap<>();
     public static void main(String[] args) {
+        timesSwitched.put(switched, 0);
+        timesSwitched.put(didntSwitch, 0);
+        timesWon.put(switched, 0);
+        timesWon.put(didntSwitch, 0);
 
         String play = "y";
         while(play.toLowerCase().contains("y")) {
@@ -54,10 +32,17 @@ public class Main {
             if(outputDoorList(doors.keySet())) System.out.println(" are your remaining choices");
             else System.out.println(" is your remaining choice");
 
-            System.out.println("Would you like to choose a new door? type y if yes");
+            System.out.println("Would you like to switch to the other door? type y if yes");
             String confirmation = scanner.next();
 
-            if(confirmation.toLowerCase().contains("y")) choice = chooseDoor(doors);
+            if(confirmation.toLowerCase().contains("y")) {
+                choice = switchDoors(choice, doors);
+                updateStatistics(switched, doors.get(choice).equals(prize));
+            } else {
+                updateStatistics(didntSwitch, doors.get(choice).equals(prize));
+            }
+
+
 
             revealWinner(doors, choice);
 
@@ -66,7 +51,14 @@ public class Main {
             System.out.println("Play again type y if yes!");
             play = scanner.next();
         }
-        System.out.println("Bye");
+        System.out.println("\n\n\nYou final statistics are:");
+
+        System.out.println("\nYou switched " + timesSwitched.get(switched) + " time(s)");
+        System.out.println("You won by switching " + timesWon.get(switched) + " time(s)");
+
+        System.out.println("\n You didn't switch " + timesSwitched.get(didntSwitch) + " time(s)");
+        System.out.println("You won by not switching " + timesWon.get(didntSwitch) + " time(s)");
+
 
     }
 
@@ -170,6 +162,23 @@ public class Main {
         return choice;
     }
 
+    public static String switchDoors(String currentChoice, HashMap<String, String> choices) {
+        if(choices.size() != 2) {
+            System.out.println("Tried to switch doors with more than 2 options left not sure what you want?");
+            return chooseDoor(choices);
+        }
+
+        for(String door : choices.keySet()) {
+            if(!door.equals(currentChoice)) {
+                System.out.println(door + " choose");
+                return door;
+            }
+        }
+        //And this last part will never be reached but if it is
+        System.out.println("Couldn't find non choose door so choose yourself");
+        return chooseDoor(choices);
+    }
+
     public static void revealExcessDoors(String chosenDoor, HashMap<String, String> doors) {
         System.out.println();
 
@@ -195,6 +204,18 @@ public class Main {
 
         if(outputDoorList(removedDoors)) System.out.println(" are all " + loss + "s!");
         else System.out.print(" is a " + loss + "\n");
+
+    }
+
+    //Takes a String for if you did switch or not should be one of the constants switched or didntSwitch
+    // and a boolean based on whether you chose the correct door
+    //Then based on that updates the HashMaps for how many times you switch and how many of those times you won
+    public static void updateStatistics(String switched, boolean won) {
+        int increment = 0;
+        if(won) increment = 1;
+
+        timesSwitched.put(switched, (timesSwitched.get(switched) + 1));
+        timesWon.put(switched, (timesWon.get(switched) + increment));
 
     }
 
